a9216920e3880c086fc9ddc598cfa9f6d3bfe0d1
smpmaneth <smpmaneth@gmail.com>
2025-03-17 10:24:17 +0530

    build(deps): update package-lock.json, package.json, App.js and MoviesGrid.js

---
+++ src/components/Watchlist.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "../styles.css";
+
+
+
+export default function Watchlist() {
+    return (
+        <div>
+            <h1>Watchlist</h1>
+        </div>
+    )
+}
